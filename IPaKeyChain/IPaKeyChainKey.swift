4aa2379f44d2648658b130f446ea75fd57b0d3d1
IPaPa <ipapamagic@gmail.com>
2015-11-02 23:42:04 +0800

    initial commit

--- IPaKeyChain/IPaKeyChainKey.swift
+++ IPaKeyChain/IPaKeyChainKey.swift
@@ -8,57 +8,6 @@
 
 import Foundation
 
-public enum IPaSecAttrKeyClass : RawRepresentable {
-    case Public,Private,Symmetric
-    
-    public init?(rawValue: String) {
-        switch rawValue {
-        case String(kSecAttrKeyClassPublic):
-            self = Public
-        case String(kSecAttrKeyClassPrivate):
-            self = Private
-        case String(kSecAttrKeyClassSymmetric):
-            self = Symmetric
-        default:
-            self = Public
-        }
-    }
-    public var rawValue: String {
-        switch self {
-        case .Public:
-            return String(kSecAttrKeyClassPublic)
-        case .Private:
-            return String(kSecAttrKeyClassPrivate)
-        case .Symmetric:
-            return String(kSecAttrKeyClassSymmetric)
-        }
-        
-    }
-}
-public enum IPaSecAttrKeyType : RawRepresentable {
-    case RSA,EC
-    public init?(rawValue: String) {
-        switch rawValue {
-        case String(kSecAttrKeyTypeRSA):
-            self = RSA
-        case String(kSecAttrKeyTypeEC):
-            self = EC
-        default:
-            self = RSA
-        }
-    }
-    public var rawValue: String {
-        switch self {
-        case .RSA:
-            return String(kSecAttrKeyTypeRSA)
-        case .EC:
-            return String(kSecAttrKeyTypeEC)
-
-        }
-        
-    }
-
-}
 
 
 class IPaKeyChainKey :IPaKeyChainKeyIdentity {
